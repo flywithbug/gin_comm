acc2b6da1512b3d18879cd71778fc8e0807bdeae
chengerfeng08824 <chengerfeng08824@hellobike.com>
2021-01-14 14:58:00 +0800

    0.0.2

--- http/handler_comm/comm.go
+++ http/handler_comm/comm.go
@@ -1,6 +1,5 @@
 package handler_comm
 
-
 import "github.com/gin-gonic/gin"
 
 type StateType int
@@ -10,11 +9,19 @@ const (
 	RouterTypeNeedAuth
 )
 
+type MethodType string
 
+const (
+	MethodPOST   MethodType = "POST"
+	MethodGET    MethodType = "GET"
+	MethodPUT    MethodType = "PUT"
+	MethodDELETE MethodType = "DELETE"
+)
 
 type GinHandleFunc struct {
-	Handler    gin.HandlerFunc
-	Method     string
-	Route      string
-	RouterGroup      *gin.RouterGroup
+	Handler     gin.HandlerFunc
+	Method      MethodType
+	Route       string
+	RouterGroup *gin.RouterGroup
+	RouterType  StateType
 }
